f3deffae2b65b0d227297c6814213691e2b79467
tarilabs <matteo.mortari@gmail.com>
2020-10-24 10:20:22 +0200

    implement listener

--- src/main/java/com/myspace/bc202010dmnlistener/MyDecisionListener.java
+++ src/main/java/com/myspace/bc202010dmnlistener/MyDecisionListener.java
@@ -1,10 +1,17 @@
 package com.myspace.bc202010dmnlistener;
 
-import java.lang.Object;
+import org.kie.dmn.api.core.event.AfterEvaluateDecisionEvent;
 
-public class MyDecisionListener extends Object {
+public class MyDecisionListener extends org.kie.dmn.core.api.event.DefaultDMNRuntimeEventListener {
 
-	public MyDecisionListener() {
+	@Override
+	public void afterEvaluateDecision(AfterEvaluateDecisionEvent event) {
+		System.out.println("Decision " + 
+							event.getDecision().getName() + 
+							" evaluated with result: " +
+							event.getResult().getDecisionResultByName(event.getDecision().getName()).getResult()
+		);
 	}
+	
 
 }
