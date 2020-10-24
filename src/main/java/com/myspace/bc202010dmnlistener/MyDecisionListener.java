61710d77c47ffef1cae5408808cdefa9d6bf3ace
krisv <>
2020-10-24 10:09:55 +0200

    {/src/main/java/com/myspace/bc202010dmnlistener/MyDecisionListener.java}

--- src/main/java/com/myspace/bc202010dmnlistener/MyDecisionListener.java
+++ src/main/java/com/myspace/bc202010dmnlistener/MyDecisionListener.java
@@ -2,7 +2,7 @@ package com.myspace.bc202010dmnlistener;
 
 import java.lang.Object;
 
-public class MyDecisionListener extends Object {
+public class MyDecisionListener {
 
 	public MyDecisionListener() {
 	}
